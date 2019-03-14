94b12258469f4f35a7a4c0bcb15c7b3f14672203
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-03-14 18:45:53 +0200

    add html files

--- gallery/views.py
+++ gallery/views.py
@@ -1,34 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the personal gallery applications')
+    return render(request, 'welcome.html')
+    
 
 def gallery_of_day(request):
     date = dt.date.today()
-  
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Gallery for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
 
-def convert_dates(dates):
+    return render(request, 'all-gallery/today-gallery.html', {"date": date,})
 
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
 
 def past_days_gallery(request,past_date):
     try:
@@ -38,15 +20,9 @@ def past_days_gallery(request,past_date):
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        assert False
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Gallery for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    if date == dt.date.today():
+        return redirect(gallery_of_day)
+
+    return render(request, 'all-gallery/past-gallery.html', {"date": date})
