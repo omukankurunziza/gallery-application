80d0a3cc281eca5e95b7cacaca0c6d32613e8c0d
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-03-15 16:15:03 +0200

    add search url

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
    
     url('^$',views.gallery_of_day,name='galleryToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name = 'pastGallery') 
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name = 'pastGallery'),
+     url(r'^search/', views.search_results, name='search_results')
 ]
