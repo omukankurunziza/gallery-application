29a34d55faac3200e17f79057b14335861f952c8
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-03-15 14:12:36 +0200

    add change make

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+    description = models.TextField()
+    def __str__(self):
+        return self.name
+class Category(models.Model):
+    name = models.CharField(max_length =30)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    image = models.CharField(max_length =30)
+    name = models.CharField(max_length =30)
+    description = models.TextField()
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+
+
